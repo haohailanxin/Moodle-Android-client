28987d3d90c94f36612c72f24aed000c0168d00f
Andrey Teteria <Paladin26@narod.ru>
2011-11-15 19:21:44 -0430

    Added login activity stub.

---
+++ MoodleAndroidClient/src/com/andreyteteria/android/moodle/activities/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.andreyteteria.android.moodle.activities;
+
+import com.andreyteteria.android.moodle.R;
+import com.andreyteteria.android.moodle.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_home);
+    }
+}
---
+++ MoodleAndroidClient/src/com/andreyteteria/android/moodle/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.andreyteteria.android.moodle.activities;
+
+import com.andreyteteria.android.moodle.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ac_login);
+	}
+	
+}
