6c744d74f8429501a808dca45f0ed0f10e423acc
Andrey Teteria <Paladin26@narod.ru>
2011-11-15 17:17:41 -0430

    Initial commit

---
+++ MoodleAndroidClient/src/com/andreyteteria/android/moodle/MoodleAndroidClientActivity.java
@@ -0,0 +1,13 @@
+package com.andreyteteria.android.moodle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MoodleAndroidClientActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
